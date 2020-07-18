c36cb9ecf256a9e6434b8e52ba8f7da91a851363
diptoroy <diptoroy54@gmail.com>
2020-07-18 19:10:51 +0600

    add

--- app/src/main/java/com/atcampus/morefriendsmorefun/Activity/MainActivity.java
+++ app/src/main/java/com/atcampus/morefriendsmorefun/Activity/MainActivity.java
@@ -2,17 +2,26 @@ package com.atcampus.morefriendsmorefun.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.atcampus.morefriendsmorefun.Fragment.FriendsFragment;
+import com.atcampus.morefriendsmorefun.Fragment.HomeFragment;
+import com.atcampus.morefriendsmorefun.Fragment.NotificationFragment;
+import com.atcampus.morefriendsmorefun.Fragment.ProfileFragment;
 import com.atcampus.morefriendsmorefun.R;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     private FirebaseAuth mFirebaseAuth;
 
@@ -22,7 +31,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView2);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
     }
+
+
 
     private void checkUserStatus(){
         FirebaseUser user = mFirebaseAuth.getCurrentUser();
@@ -56,4 +70,30 @@ public class MainActivity extends AppCompatActivity {
         checkUserStatus();
         super.onStart();
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_friends:
+                            selectedFragment = new FriendsFragment();
+                            break;
+                        case R.id.nav_notification:
+                            selectedFragment = new NotificationFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, selectedFragment).commit();
+                        return true;
+                    }
+                };
+
 }
